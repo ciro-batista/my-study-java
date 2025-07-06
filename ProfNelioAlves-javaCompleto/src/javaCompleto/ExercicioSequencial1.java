83e3e67676137d16c34db424ece43b04fb07f269
Ciro Batista <cirobatista@outlook.com>
2025-07-06 20:59:45 -0300

    ProfNelioAlves

---
+++ ProfNelioAlves-javaCompleto/src/javaCompleto/ExercicioSequencial1.java
@@ -0,0 +1,27 @@
+package javaCompleto;
+
+import java.util.Scanner;
+
+public class ExercicioSequencial1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		Scanner sc = new Scanner (System.in); 
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int c = a + b;
+		
+		
+		sc.close();
+		
+		
+		System.out.println("SOMA = "+ c);
+
+	}
+
+}
---
+++ ProfNelioAlves-javaCompleto/src/javaCompleto/ExercicioSequencial2.java
@@ -0,0 +1,32 @@
+package javaCompleto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioSequencial2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner (System.in);
+		
+		double pi = 3.14159;
+				
+		double r = sc.nextDouble();
+		
+		double r2 = Math.pow(r, 2);
+		
+				
+		double area = pi * r2;
+		
+		sc.close();
+		
+		System.out.printf("A=%.4f", area);
+		
+		
+
+	}
+
+}
